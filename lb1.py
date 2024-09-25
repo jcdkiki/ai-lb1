21a7acd04fc0c4a7cc382914dc43341c79e2e3ae
Gorsky Kirill <kirillgorskiy@gmail.com>
2024-09-25 21:59:18 +0300

    done

--- lb1.py
+++ lb1.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from scipy import integrate
 import itertools
 import math
 import os
+import shutil
 
 DEFAULT_RANDOMNESS = 10
 DEFAULT_N_POINTS = 64
@@ -18,7 +20,7 @@ DEFAULT_N_CLUSTERS = 3
 def rand_vector(r):
     return np.array([
         random.uniform(-r, r), random.uniform(-r, r), random.uniform(-r, r),
-        random.uniform(-r, r), random.uniform(-r, r)]
+        random.uniform(-r, r), random.uniform(-r, r)], dtype='float64'
     )
 
 def generate_line_point(x):
@@ -35,24 +37,21 @@ def generate_plane_point(x, y):
 def generate_random_point():
     return rand_vector(400) + np.array([100, 23, -50, 1, -100])
 
-def process_dataset(data, n_components=2):
-    data_scaled = StandardScaler().fit_transform(data)
-    return PCA(n_components).fit_transform(data_scaled)
+def generate_polynomial_point(x):
+    return np.array([10*x, 13*x*x, 54*x, 5*x + 30*x*x, 90*x*x*x*x]) + np.array([432, 54, 12321, 76, -1000])
 
 def clusterize(data, k, seed):
     kmeans = KMeans(n_clusters=k, random_state=seed)
     kmeans.fit(data)
-    return kmeans.labels_, kmeans.cluster_centers_
+    return kmeans.labels_, kmeans.cluster_centers_, kmeans.inertia_
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--randomness", type=int, default=DEFAULT_RANDOMNESS)
     parser.add_argument("-s", "--seed", type=int, default=int(time.time()))
-    parser.add_argument("-n", type=int, default=DEFAULT_N_POINTS)
     parser.add_argument("--line-scale", type=float, default=DEFAULT_LINE_SCALE)
     parser.add_argument("-c", "--clusters", type=int, default=DEFAULT_N_CLUSTERS)
     parser.add_argument("-S", "--cluster-seed", type=int, default=int(time.time()))
-    #parser.add_argument("-i", "--inverse", action='store-true')
 
     args = parser.parse_args()
 
@@ -60,33 +59,46 @@ def main():
     datasets = {}
 
     points = []
-    for x in range(math.ceil(math.sqrt(args.n))):
-        for y in range(math.ceil(math.sqrt(args.n))):
+    for x in range(8):
+        for y in range(8):
             points.append(generate_plane_point(x, y))
-    datasets["plane"] = np.array(points[0:args.n])
-    
-    datasets["line"] = np.array([generate_line_point(x * args.line_scale) for x in range(args.n)])
+    datasets["plane"] = np.array(points, dtype='float64')
 
-    datasets["random"] = np.array([generate_random_point() for _ in range(args.n)])
+    datasets["line"] = np.array([generate_line_point(x * args.line_scale) for x in range(50)], dtype='float64')
 
+    datasets["random"] = np.array([generate_random_point() for _ in range(50)], dtype='float64')
+
+    datasets["polynomial"] = np.array([generate_polynomial_point((x - 20) * args.line_scale) for x in range(50)], dtype='float64')
+
+    shutil.rmtree("plots", ignore_errors=True)
     os.makedirs("plots", exist_ok=True)
 
     for name, data in datasets.items():
-        for i, j in itertools.product(range(args.n), range(5)):
-            data[i][j] += random.uniform(-1, 1) * args.randomness
-        result = process_dataset(data)
+        for i in (range(len(data))):
+            data[i] += rand_vector(args.randomness)
+        
+        data_scaled = StandardScaler().fit_transform(data)
+        result = PCA(2).fit_transform(data_scaled)
+
         print(result)
 
-        plt.cla()
+        summary = {}
 
-        for k in range(2, args.clusters + 1):
-            labels, centroids = clusterize(result, args.clusters, args.cluster_seed)
+        for k in range(1, args.clusters + 1):
+            plt.cla()
+            labels, centroids, inertia = clusterize(result, k, args.cluster_seed)
             for i in range(k):
                 plt.plot(result[labels == i, 0], result[labels == i, 1], 'o')
 
-            plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='red', marker='X')
+            summary[k] = inertia
 
+            plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='red', marker='X')
             plt.savefig(f"plots/{name}-{k}.png")
+        
+        plt.cla()
+        plt.plot(list(summary.keys()), list(summary.values()))
+        plt.savefig(f"plots/{name}-summary.png")
+
 
 if __name__ == "__main__":
     main()
